62cc5059844bff8c9bc903812465c17ccf2bf91f
abdalaziznahla <143839951+abdalaziznahla@users.noreply.github.com>
2024-06-27 02:53:04 +0300

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abdalaziz Mostafa",
+  title: "Dentist and 2D Animator",
+  email: "abdalaziznahla@gmail.com",
+  gitHub: "abdalaziznahla",
+  instagram: "abdalaziz_nahla10",
+  linkedIn: "abdalaziznahla",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "abdalaziznahla",
+  youTube: "abdalaziznahla",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,18 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "This is my profile.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Dentist",
+  "Gamer",
+  "2D animator",
+  "Pet owner",
+  "Tech enthuist",
   "Graphic design",
 ];
 
